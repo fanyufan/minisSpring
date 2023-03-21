160041621839da696a1a91102b0e0e5811830ba4
80575368 <sam.yf.fan@pccw.com>
2023-03-21 17:01:51 +0800

    version 01｜原始IoC：如何通过BeanFactory实现原始版本的IoC容器?
    初步构建起了 MiniSpring 的核心部分：Bean 和 IoC

--- src/main/java/com/minis/Test1.java
+++ src/main/java/com/minis/Test1.java
@@ -1,9 +1,12 @@
 package com.minis;
 
+import com.minis.beans.BeansException;
+import com.minis.context.ClassPathXmlApplicationContext;
+import com.minis.core.ClassPathXmlResource;
 import com.minis.test.AService;
 
 public class Test1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BeansException {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         AService aService = (AService) ctx.getBean("aservice");
         aService.sayHello();
---
+++ src/main/java/com/minis/context/ClassPathXmlApplicationContext.java
@@ -0,0 +1,29 @@
+package com.minis.context;
+
+import com.minis.BeanDefinition;
+import com.minis.beans.BeanFactory;
+import com.minis.beans.BeansException;
+import com.minis.beans.SimpleBeanFactory;
+import com.minis.beans.XmlBeanDefinitionReader;
+import com.minis.core.ClassPathXmlResource;
+import com.minis.core.Resource;
+
+public class ClassPathXmlApplicationContext implements BeanFactory {
+    BeanFactory beanFactory;
+    public ClassPathXmlApplicationContext(String fileName) {
+        Resource resource = new ClassPathXmlResource(fileName);
+        BeanFactory beanFactory = new SimpleBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions(resource);
+        this.beanFactory = beanFactory;
+    }
+    @Override
+    public Object getBean(String beanName) throws BeansException {
+        return this.beanFactory.getBean(beanName);
+    }
+
+    @Override
+    public void registerBeanDefinition(BeanDefinition beanDefinition) {
+        this.beanFactory.registerBeanDefinition(beanDefinition);
+    }
+}
