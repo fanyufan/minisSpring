89bd28061819936b6d5260409f53347d7e01ed07
80575368 <sam.yf.fan@pccw.com>
2023-03-21 14:10:03 +0800

    version 01｜原始IoC：如何通过BeanFactory实现原始版本的IoC容器?

---
+++ src/main/java/com/minis/Test1.java
@@ -0,0 +1,11 @@
+package com.minis;
+
+import com.minis.test.AService;
+
+public class Test1 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+        AService aService = (AService) ctx.getBean("aservice");
+        aService.sayHello();
+    }
+}
